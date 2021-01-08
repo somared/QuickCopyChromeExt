bdf2e86ee3d639ced20694d88d15064716c6c923
sm1810 <soyabmaredia@gmail.com>
2021-01-07 16:48:01 -0800

    Added images

--- src/UserDataList.js
+++ src/UserDataList.js
@@ -1,16 +1,25 @@
 import List from '@material-ui/core/List';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import UserData from "./UserData";
 
 const UserDataList = ({dataList, removeItem, showMessage}) => {
     return(
-        <div>
-            <List dense="true" style={{width:"350px"}}>
-                {
-                    dataList.map(d => {
-                        return(<UserData data={d} deleteItem={removeItem} key={d.id} showMessage={showMessage}/>)
-                    })
-                }
-            </List>
+        <div style={{width:"350px"}}>
+            {
+                (dataList.length > 0) ?
+                    <List dense="true" >
+                        {
+                            dataList.map(d => {
+                                return(<UserData data={d} deleteItem={removeItem} key={d.id} showMessage={showMessage}/>)
+                            })
+                        }
+                    </List>
+                    :
+                    <Card>
+                        <CardContent>No data is configured. Add text using below form</CardContent>
+                    </Card>
+            }
         </div>
     );    
 }

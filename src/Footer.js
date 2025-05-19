0e49f1ec2d2750cc40baed5ecae7dd600d7d4cb5
somared <soyabmaredia@gmail.com>
2025-05-19 08:14:31 -0700

    Added help icon

--- src/Footer.js
+++ src/Footer.js
@@ -4,6 +4,7 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import IconButton from '@mui/material/IconButton';
+import { Help } from '@mui/icons-material';
 
 const Footer = () => {
     
@@ -17,11 +18,13 @@ const Footer = () => {
                 <IconButton href="https://github.com/somared/QuickCopyChromeExt" size="small" color="primary" edge="end" target="_blank" >
                     <GitHubIcon sx={{ color: 'black' }} fontSize="small"/>
                 </IconButton>
+                <IconButton href="https://github.com/somared/QuickCopyChromeExt" size="small" color="primary" edge="end" target="_blank" >
+                    <Help sx={{ color: 'black' }} fontSize="small"/>
+                </IconButton>
                 <Box sx={{ alignContent: 'center'}}>
                     v1.1.0
                 </Box>
             </Box>
-            
         </StyledDiv>
     );
 }
